a1bf2a14a729088e9e5e4471a08aae60148e978e
Win Feigle <win.feigle@gmail.com>
2022-05-18 20:49:46 -0600

    commit tree 0df33dd06bb2786fe4400aba135d500937084511

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: 'name',
+    streetAddress: 'street address',
+}
+
+const updateEmployeeWithKeyAndValue = (obj, key, value) => {
+    const newObj = {...obj}
+    newObj[key] = value;
+    return newObj;
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (obj, key, value) => {
+    obj[key] = value;
+    return obj;
+}
+
+const deleteFromEmployeeByKey = (obj, key) => {
+    const newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+}
+
+const destructivelyDeleteFromEmployeeByKey = (obj, key) => {
+    delete obj[key];
+    return obj;
+}
+
